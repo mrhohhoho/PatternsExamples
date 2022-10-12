b9e9145a48a39b771d4b8b0b867929f83ff4b9e2
mrhohhoho <mrhohhoho@gmail.com>
2022-10-12 19:39:02 +0300

    sigleton module first

---
+++ singleton_examples/src/DemoSingleThread.java
@@ -0,0 +1,8 @@
+public class DemoSingleThread {
+    public static void main(String[] args) {
+        System.out.println("Hello world! \n");
+
+        Singleton singleton = Singleton.getInstance("FIRST");
+        S
+    }
+}
